ed49a501a1826495caef287e59f132335746d95a
ahmedhlulu <100189769+ahmedhlulu@users.noreply.github.com>
2022-05-10 09:34:17 +0300

    Initial Commit

---
+++ My First Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My First Project
+//
+//  Created by Ahmed on 10/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My First Project/My_First_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_ProjectApp.swift
+//  My First Project
+//
+//  Created by Ahmed on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct My_First_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
